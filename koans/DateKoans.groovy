81ae2078a6170ebf8db1faf860bfdbb9edd68a4d
Dani Latorre <danilat83@gmail.com>
2011-02-16 13:45:54 +0100

    cleaned some Date koans to SpringIO

--- koans/DateKoans.groovy
+++ koans/DateKoans.groovy
@@ -27,26 +27,6 @@ class DateKoans extends Koans {
 		assert __ == date.minutes
 	}
 	
-	void testCreateDateViaConstructor() {
-		// Java Constructor
-		def date = new Date(101,8,11)
-		assert __ == date.class.name
-		assert __ == date.year
-		assert __ == date.month
-		assert __ == date.date
-		assert __ == date.hours
-		assert __ == date.minutes
-		
-		// Groovy Named Parameters
-		def date2 = new Date(year: 101, month: SEPTEMBER, date: 11, hours:0, minutes: 0, seconds: 0)
-		assert __ == date2.class.name
-		assert __ == date2.year
-		assert __ == date2.month
-		assert __ == date2.date
-		assert __ == date2.hours
-		assert __ == date2.minutes
-	}
-	
 	void testCreateDateFromCalendar() {
 		def cal = Calendar.instance
 		cal.set(year: 2001, month: SEPTEMBER, date: 11, hourOfDay: 0, minute: 0, second: 0)
@@ -147,14 +127,6 @@ class DateKoans extends Koans {
 		assert __ == dateIncrement.hours
 		assert __ == dateIncrement.minutes
 		assert __ == dateIncrement.seconds
-		
-		def dateDecrement = --date
-		assert __ == dateDecrement.year
-		assert __ == dateDecrement.month
-		assert __ == dateDecrement.date
-		assert __ == dateDecrement.hours
-		assert __ == dateDecrement.minutes
-		assert __ == dateDecrement.seconds
 	}
 	
 	// Compare Dates
